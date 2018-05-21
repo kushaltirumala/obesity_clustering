5389dd712397ed976fb66d04c4fd204057590bb6
Kushal Tirumala <kushaltirumala99@gmail.com>
2018-05-21 03:55:13 -0700

    kmeans not working for some reason

--- run_tsne.py
+++ run_tsne.py
@@ -10,11 +10,12 @@ import matplotlib.mlab as mlab
 from matplotlib.ticker import NullFormatter
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+from sklearn.cluster import KMeans
 
 # trying to compare the tsne with pca
 # will also compare runtimes
 
-with_images = True
+with_images = False
 
 def load_data(directory):
     arr = []
@@ -34,11 +35,10 @@ def load_data(directory):
 
     return ans, arr
 
-def plot_images(embeddings, arr, plot_images=False, save_name=None, show_image=True):
+def plot_images(embeddings, arr, plot_images=False, save_name=None, show_image=True, pred=None, centers=None):
         fig = plt.figure()
         ax = fig.add_subplot(111)
         # colors = cm.Spectral(np.linspace(0, 1, num_classes))
-
         xx = embeddings[:, 0]
         yy = embeddings[:, 1]
 
@@ -57,7 +57,12 @@ def plot_images(embeddings, arr, plot_images=False, save_name=None, show_image=T
         # plot the 2D data points
         for i in range(embeddings.shape[0]):
             print("PLOTTING " + str(i))
-            ax.scatter(xx[i], yy[i], s=10)
+            ax.scatter(xx[i], yy[i], c=pred, s=50)
+
+        if centers:
+            ax.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
+
+
 
         ax.xaxis.set_major_formatter(NullFormatter())
         ax.yaxis.set_major_formatter(NullFormatter())
@@ -78,4 +83,13 @@ data, arr = load_data("test/")
 embeddings = tsne.fit_transform(data, arr)
 
 # plot the initial embeddings
-plot_images(embeddings, arr, plot_images=with_images, save_name="plots/pre_clustering_plot_0.3_no_images.png", show_image=True)
+plot_images(embeddings, arr, plot_images=True, save_name="plots/pre_clustering_plot_0.03_no_images.png", show_image=False)
+
+# k-means example
+kmeans = KMeans(n_clusters=4)
+kmeans.fit(embeddings)
+y_kmeans = kmeans.predict(embeddings)
+centers = kmeans.cluster_centers_
+plot_images(embeddings, arr, plot_images=True, save_name="plots/pre_clustering_plot_kmeans_4_images.png", show_image=True, pred=y_kmeans, centers=centers)
+
+
